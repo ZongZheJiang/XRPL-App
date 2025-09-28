91cddc9a9ab15b2815868fba52a34772b7bbdda1
zjiang026-sudo <zjiang026@berkeley.edu>
2025-09-27 18:31:11 -0700

    removed add client modifier on supabaseClient.ts

--- src/app/homepage/create-smart-contract/page.tsx
+++ src/app/homepage/create-smart-contract/page.tsx
@@ -2,11 +2,18 @@
 
 import React, { useState } from "react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from 'react';
 import CreateSmartContract from "@/components/CreateSmartContract"; // Adjust the import path as needed
 
 function Escrow() {
   return (
-    <CreateSmartContract />
+    <div>
+      <h1>Create Smart Contract</h1>
+      <Suspense fallback={<div>Loading form...</div>}>
+        <CreateSmartContract />
+      </Suspense>
+    </div>
+
   );
 }
 
